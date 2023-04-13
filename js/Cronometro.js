240dd33269899a30f5d43d8e225221f5b694c8d3
nicoleonard <nikolasleonard@gmail.com>
2023-04-13 08:37:10 -0300

    cronometro en marcha

--- js/Cronometro.js
+++ js/Cronometro.js
@@ -1,5 +1,11 @@
 let cronometro = document.querySelector("#cronometro");
-let tiempo = new Date();
-setInterval(()=>{
-    cronometro.innerHTML = tiempo.toLocaleTimeString('en-US')
-},100);
+let boton_iniciar = document.querySelector("#boton_iniciar")
+let boton_restablecer = document.querySelector("#boton_restablecer")
+let boton_pausar = document.querySelector("#boton_pausar")
+
+boton_iniciar.addEventListener("click", ()=>{
+    let tiempo_inicial = Date.now();
+    setInterval(()=>{
+        cronometro.innerHTML = `${parseInt(Date.now() - tiempo_inicial)/1000} Segundos`;
+    },100);
+})
