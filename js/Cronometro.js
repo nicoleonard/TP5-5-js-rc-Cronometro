51caed7a02df2eb1f6fd457f832a64eb126f8cf5
nicoleonard <nikolasleonard@gmail.com>
2023-04-13 08:56:19 -0300

    version inestable

--- js/Cronometro.js
+++ js/Cronometro.js
@@ -2,10 +2,24 @@ let cronometro = document.querySelector("#cronometro");
 let boton_iniciar = document.querySelector("#boton_iniciar")
 let boton_restablecer = document.querySelector("#boton_restablecer")
 let boton_pausar = document.querySelector("#boton_pausar")
+let intervalo = "";
+let tiempo_actual = "";
 
 boton_iniciar.addEventListener("click", ()=>{
-    let tiempo_inicial = Date.now();
-    setInterval(()=>{
-        cronometro.innerHTML = `${parseInt(Date.now() - tiempo_inicial)/1000} Segundos`;
+    tiempo_inicial = Date.now();
+    intervalo = setInterval(()=>{
+        console.log(tiempo_actual)
+        cronometro.innerHTML = `${(parseInt(Date.now() - tiempo_inicial)/1000) + tiempo_actual} Segundos`;
     },100);
 })
+
+boton_pausar.addEventListener("click", ()=>{
+    tiempo_actual=(parseFloat(cronometro.innerHTML));
+    clearInterval(intervalo);
+})
+
+boton_restablecer.addEventListener("click", ()=>{
+    tiempo_actual="";
+    cronometro.innerHTML = ``
+})
+
